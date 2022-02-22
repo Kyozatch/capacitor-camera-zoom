242139b2881eeb7aa81ba31aa305317daae28b27
Alexandre Lecut <alexandre.lecut@ama.bzh>
2022-02-22 09:48:01 +0100

    fix capabilities settings

--- src/components/cameraVue.vue
+++ src/components/cameraVue.vue
@@ -1,58 +1,59 @@
 <template>
   <div class="content">
-    <video ref="videoTag" class="main-video" playsinline autoplay></video>
-    <button @click="initializeVideoStream">Init Video</button>
-    <input type="range" />
+    <video
+      ref="videoTag"
+      class="main-video"
+      playsinline
+      autoplay
+    ></video>
+    <input type="range">
   </div>
 </template>
 
 <script>
 export default {
   name: 'CameraVue',
-  methods: {
-    initializeVideoStream() {
-      const constraints = {
-        audio: true,
-        video: { width: 1280, height: 720, zoom: true },
-      };
-      navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function (mediaStream) {
-          const track = mediaStream.getVideoTracks()[0];
-          const capabilities = track.getCapabilities();
-          const settings = track.getSettings();
-          const input = document.querySelector('input[type="range"]');
-          console.log(capabilities);
-          console.log(settings);
-
-          var video = document.querySelector('video');
-          video.srcObject = mediaStream;
-          video.onloadedmetadata = function () {
-            video.play();
-          };
-
-          input.min = capabilities.zoom.min;
-          input.max = capabilities.zoom.max;
-          input.step = capabilities.zoom.step;
-          input.value = settings.zoom;
-          input.oninput = function (event) {
-            track.applyConstraints({
-              advanced: [{ zoom: parseInt(event.target.value) }],
-            });
-          };
-          input.hidden = false;
-        })
-        .catch(function (err) {
-          console.log(err.name + ': ' + err.message);
-        });
-    },
+  mounted() {
+    this.initializeVideoStream();
   },
-};
+  methods: {
+   initializeVideoStream() {
+      const constraints = { audio: true, video: { width: 1280, height: 720, zoom: true} };
+      navigator.mediaDevices.getUserMedia(constraints)
+      .then(function(mediaStream) {
+        const track = mediaStream.getVideoTracks()[0];
+        const capabilities = track.getCapabilities();
+        const settings = track.getSettings();
+        const input = document.querySelector('input[type="range"]')
+        console.log(capabilities);
+        console.log(settings);
+
+        var video = document.querySelector('video');
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = function() {
+          video.play();
+        };
+
+        input.min = 1;
+        input.max = 4;
+        input.step = 1;
+        input.value = settings.zoom;
+        input.oninput = function(event) {
+          console.log('zoom set to: ', event.target.value)
+          track.applyConstraints({advanced: [ {zoom: parseInt(event.target.value)} ]});
+        }
+        input.hidden = false;
+      })
+      .catch(function(err) { console.log(err.name + ": " + err.message); });
+    },
+},
+}
+
 </script>
 
 <style scoped>
 video {
-  width: 70%;
-  background: rgba(0, 0, 0, 0.2);
+    width: 70%;
+    background: rgba(0, 0, 0, 0.2);
 }
 </style>
